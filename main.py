65b8b101a67115fc8949e1214fbf80c452a60d3e
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-06-04 21:10:23 +0300

    set 1st attacker is placed correctly

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from game import Game
 
 def main():
     game = Game()
-    game.start_round()
+
     game.set_attacker_for_1st_round()
+    game.start_round()
 
     while game.status == "in_progress":
         game.start_round()
